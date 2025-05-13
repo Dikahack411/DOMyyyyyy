295c641909ecc34bf21416aa79b91e5b0b87d77b
“Dikahack411” <2g.dikachi@gmail.com>
2025-05-13 17:22:36 +0100

    Comity

---
+++ NEW NEW/app.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+const menuIcon = document.getElementById("menu-icon");
+
+toggleBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+
+  //   SWITCH BETWEEN MENU ICON AND CLOSE ICON
+});
